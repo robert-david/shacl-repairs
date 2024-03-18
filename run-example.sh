5135e4a3a6f4f0bd14ecd98214f2256e4a9c85c2
Robert David <robert.david@semantic-web.com>
2024-03-18 23:41:03 +0100

    Fixed run-example.sh

--- run-example.sh
+++ run-example.sh
@@ -1,2 +1,2 @@
-java -jar ./shacl-repairs-1.1.jar org.shacl.repairs.Main src/test/resources/org/shacl/repairs/tests/example_data.ttl src/test/resources/org/shacl/repairs/tests/example_shapes.ttl shacl-repairs/src/test/resources/org/shacl/repairs/tests/example_rules.pl
-clingo src/test/resources/org/shacl/repairs/tests/example_rules.pl --opt-mode=optN -n 10 --quiet=1
+java -jar ./target/shacl-repairs-1.2.0-SNAPSHOT.jar src/test/resources/org/shacl/repairs/tests/example_data.ttl src/test/resources/org/shacl/repairs/tests/example_shapes.ttl src/test/resources/org/shacl/repairs/tests/example_rules.pl
+clingo src/test/resources/org/shacl/repairs/tests/example_rules.pl --opt-mode=optN -n 100 -t 3 --quiet=1
