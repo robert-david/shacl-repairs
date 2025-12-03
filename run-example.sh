cbcd0e9d3654a9a8bb1088a876ea1591678db111
Robert David <9427084@gmail.com>
2025-12-03 12:05:55 +0100

    Updated version of jar

--- run-example.sh
+++ run-example.sh
@@ -1,2 +1,2 @@
-java -jar ./target/shacl-repairs-1.2.0-SNAPSHOT.jar org.shacl.repairs.Main src/test/resources/org/shacl/repairs/tests/example_data.ttl src/test/resources/org/shacl/repairs/tests/example_shapes.ttl src/test/resources/org/shacl/repairs/tests/example_rules.pl
-clingo src/test/resources/org/shacl/repairs/tests/example_rules.pl --opt-mode=optN -n 100 --quiet=1
+java -jar ./target/shacl-repairs-1.2.1-SNAPSHOT.jar org.shacl.repairs.Main src/test/resources/org/shacl/repairs/tests/example_data.ttl src/test/resources/org/shacl/repairs/tests/example_shapes.ttl src/test/resources/org/shacl/repairs/tests/example_rules.pl
+clingo src/test/resources/org/shacl/repairs/tests/example_rules.pl --opt-mode=optN -n 0 --quiet=1
